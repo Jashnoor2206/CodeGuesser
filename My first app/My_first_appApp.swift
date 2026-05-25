bd7f3335fcfb352d3567a4d04681be2997ae3fb7
Jashnoor Singh <jashnoorsingh2005@gmail.com>
2026-05-25 16:33:02 +0530

    Initial Commit

---
+++ My first app/My_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_first_appApp.swift
+//  My first app
+//
+//  Created by Jashnoor Singh on 25/05/26.
+//
+
+import SwiftUI
+
+@main
+struct My_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
